76dc51db28459ff9ecf04b69d2b48ae64d642aa8
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2018-03-05 10:41:24 +0530

    main : added clock in debug mode

--- main.c
+++ main.c
@@ -5,11 +5,24 @@
 #include "parser.h"
 #include "display.h"
 
+#ifdef DEBUG
+#include <time.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
 #include <inttypes.h>
+
+#ifdef DEBUG
+
+static void printTime(clock_t start, clock_t end, const char *job){
+    dbg( ANSI_FONT_BOLD ANSI_COLOR_CYAN "%s " ANSI_COLOR_RESET 
+            "took " ANSI_FONT_BOLD ANSI_COLOR_GREEN "%f" ANSI_COLOR_RESET
+            " seconds", job, (double)(end - start)/CLOCKS_PER_SEC);
+}
+
+#endif
 
 int main(){
     VirtualMachine *machine = rm_new();
@@ -27,17 +40,23 @@ int main(){
     dbg("===== Source =====");
     printf("\n%s\n", source);
     dbg("===== Scanning =====\n");
+    clock_t start = clock();
 #endif
     TokenList l = tokens_scan(source);
 #ifdef DEBUG
+    clock_t end = clock();
+    printTime(start, end, "Scanning");
     dbg("===== Tokens =====\n");
     lexer_print_tokens(l);
     printf("\n");
     dbg("===== Parsing and Compiling =====\n");
+    start = clock();
 #endif
     if(l.hasError==0){
         if(parse_and_emit(l, machine->memory, 1024, 0)){
 #ifdef DEBUG
+            end = clock();
+            printTime(start, end, "Parsing");
             uint32_t offset = 0;
             printf("\n");
             dbg("===== Compiled chunk =====\n");
@@ -47,7 +66,14 @@ int main(){
             dbg("===== Executing =====\n");
 #endif
             fflush(stdout);
+#ifdef DEBUG
+            start = clock();
+#endif
             rm_run(machine, 0);
+#ifdef DEBUG
+            end = clock();
+            printTime(start, end, "Execution");
+#endif
             printf("\n");
         }
         else
